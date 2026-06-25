210b7a6db8dbfe9e9d060d4989ce43580d8656b9
darshitaofficial123-del <darshitaofficial123@gmail.com>
2026-06-25 14:43:24 +0530

    Add files via upload

---
+++ WiproAssignmentProject.py
@@ -0,0 +1,10 @@
+# Program to suggest a mode of transport based on distance
+
+distance = int(input("How far would you like to travel in miles? "))
+
+if distance < 3:
+    print("I suggest Bicycle to your destination")
+elif distance < 300:
+    print("I suggest Motor-Cycle to your destination")
+else:
+    print("I suggest Super-Car to your destination")
