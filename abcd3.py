ace0d203223322ec892be95d9f6426b3904382bc
darshitaofficial123-del <darshitaofficial123@gmail.com>
2026-06-28 22:55:12 +0530

    Add files via upload

---
+++ abcd3.py
@@ -0,0 +1,13 @@
+def sum_list(numbers: list) -> int:
+    """
+    Returns the sum of all numbers in a list.
+    """
+    return sum(numbers)
+
+
+# Sample usage
+sample_list = [8, 2, 3, 0, 7]
+print("Sum:", sum_list(sample_list))  # Expected Output: 20
+
+
+
