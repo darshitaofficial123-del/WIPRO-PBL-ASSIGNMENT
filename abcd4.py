51158524a1fb8feb41c1ce095ff19aecdf671474
darshitaofficial123-del <darshitaofficial123@gmail.com>
2026-06-28 22:56:46 +0530

    Add files via upload

---
+++ abcd4.py
@@ -0,0 +1,11 @@
+def reverse_string(s: str) -> str:
+    """
+    Returns the reverse of the given string.
+    """
+    return s[::-1]
+
+
+# Sample usage
+sample_str = "1234abcd"
+print("Reversed string:", reverse_string(sample_str))  
+# Expected Output: "dcba4321"
